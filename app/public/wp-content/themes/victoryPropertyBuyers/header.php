be2c7e4f8de53ddcac1bd3bdfd0fd1b246b0cd55
Codee J Palmer <codeecodes92@gmail.com>
2020-03-09 14:21:53 -0700

    Started on hero and how it works section

--- app/public/wp-content/themes/victoryPropertyBuyers/header.php
+++ app/public/wp-content/themes/victoryPropertyBuyers/header.php
@@ -11,6 +11,7 @@
    </div>
         <nav class="header__nav">
           <ul class="header__list">
+       <div class="header__list-logo"></div>
             <li class="header__list-item"><a href="<?php echo site_url('/about-us')?>">About Us</a></li>
             <li class="header__list-item"><a href="<?php echo site_url('/how-it-works')?>">How It Works</a></li>
             <li class="header__list-item"><a href="<?php echo site_url('/testimonials')?>">Testimonials</a></li>
--- app/public/wp-content/themes/victoryPropertyBuyers/index.php
+++ app/public/wp-content/themes/victoryPropertyBuyers/index.php
@@ -2,13 +2,18 @@
 get_header();
 ?>
 <div class="hero">
-<div class="hero__overlay">
-    <h1 class="hero__overlay-text">
-            <strong>We Buy Houses California</strong>
+  <div class="hero__overlay">
+   <h1 class="hero__overlay-text">
+    <strong>We Buy Houses California</strong>
+  </h1>
+ </div>
+</div>
+<div class="hero__bottom">
+    <h1 class="hero__bottom-text">
+        <strong>A Simple Fast Way To Sell Your House</strong>
     </h1>
 </div>
-
-</div>
+<div class=""></div>
 <?php
 get_footer();
 ?>
