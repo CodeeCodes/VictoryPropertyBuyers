192b9ba1a1d1705dcdd5b7fc9e3ee520db09e1b3
Codee J Palmer <codeecodes92@gmail.com>
2020-03-09 17:14:13 -0700

    Added reason, stressFree sections, added scss files for styling to all

--- app/public/wp-content/themes/victoryPropertyBuyers/index.php
+++ app/public/wp-content/themes/victoryPropertyBuyers/index.php
@@ -17,6 +17,64 @@ get_header();
      <h1 class="how__text">
            <strong>How It Works</strong>
     </h1>
+        <div class="how__div"><p class="how__div-text"> 
+            Step 1:  <button class="how__div-button">CASH OFFER</button> Click the button to submit your contact details and the property address of the home you wish to sell.  
+            We value your privacy and will keep communication completely professional and not spam you. Or if you’d like to speak with us directly
+            about your situation, call us at 619-900-1080.
+        </p>
+    </div>
+    <div class="how__div"><p class="how__div-text"> 
+            Step 2:  After we review the property details, we can call you back with our fair price cash offer.
+        </p>
+    </div>
+    <div class="how__div"><p class="how__div-text"> 
+            Step 3:  "Content coming-------------------------------------------------------------------------
+            -------------------------------------------------------------------------------------------------"
+        </p>
+    </div>
+</div>
+<div class="stressFree">
+      <h1 class="stressFree__text">
+           <strong>We Have You Covered</strong>
+    </h1>
+        <div class="stressFree__div"><p class="stressFree__div-text"> 
+          We understand selling your home can be a stressful time, especially if you are under financial and time constraints. 
+          Our offers are completely free of obligation.  We are here to help you find a solution and “not to judge” you on your 
+          current situation. You can reveal anything to us, and no matter what your needs are, we can offer a deal.
+          Our biggest mission is to find a win-win solution. We just want to make this process as simple and comfortable as possible.
+          There is no pressure or obligation to close with us, even if you are unsure, give us a call: 619-900-1080 and we’d love to 
+          speak with you about your situation and see what we can do for you.  
+        </p>
+    </div>
+</div>
+<div class="reason">
+    <h1 class="reason__heading">Whatever the Reason, Whatever the Current State</h1>
+    <p class="reason__div-text"> 
+       We are interested in buying your house.  Here are some of the reasons people want to sell their home to us.</p>
+    <div class="reason__flex">
+        <div class="reason__div--left">
+        <ul class="reason__list">
+            <li class="reason__list-item"> Liquidating assets</li>
+<li class="reason__list-item"> Job Loss or Change</li>
+<li class="reason__list-item">Extensive Repairs</li> 
+<li class="reason__list-item">Hoarding</li> 
+<li class="reason__list-item">Liens</li> 
+<li class="reason__list-item">Problem Tenants</li> 
+<li class="reason__list-item">Facing possible foreclosure</li> 
+<li class="reason__list-item">Death of a loved one</li> 
+
+    </div>
+    <div class="reason__div--right">
+          <ul class="reason__list">
+<li class="reason__list-item">Trouble Selling or not wanting to deal with structural or repair issues</li> 
+<li class="reason__list-item">Behind on Payments or Taxes</li> 
+<li class="reason__list-item">Ugly House</li> 
+<li class="reason__list-item">Title Issues</li> 
+<li class="reason__list-item">Need to sell an inherited property quickly</li> 
+<li class="reason__list-item">Bankruptcy</li> 
+        </ul>
+    </div>
+    </div>
 </div>
 <?php
 get_footer();
