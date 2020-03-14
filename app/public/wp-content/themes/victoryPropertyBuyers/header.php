e436a60b10de862330136ac008af054313906b1a
Codee J Palmer <codeecodes92@gmail.com>
2020-03-13 19:16:22 -0700

    Working on mobile and tablet styling for all sections

--- app/public/wp-content/themes/victoryPropertyBuyers/header.php
+++ app/public/wp-content/themes/victoryPropertyBuyers/header.php
@@ -12,7 +12,6 @@
         <a class="header__image" href="<?php echo site_url('/') ?>"><img class="header__image" src="/wp-content/themes/victoryPropertyBuyers/images/victory-property-buyers-logo.jpg" alt="logo"></a>
         <li class="header__list-item"><a href="<?php echo site_url('/') ?>">Home</a></li>
         <li class="header__list-item"><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
-        <li class="header__list-item"><a href="<?php echo site_url('/testimonials') ?>">Testimonials</a></li>
         <li class="header__list-item"><a href="<?php echo site_url('/blog') ?>">Blog</a></li>
       </ul>
     </nav>
